449bb2d2153f3030a113adbf100ff8464ea13697
DVD <gordondmitrii@gmail.com>
2023-10-05 20:17:59 +0300

    initial compiled space

--- src/compilers/codeParts/unitControls/Control.java
+++ src/compilers/codeParts/unitControls/Control.java
@@ -35,6 +35,8 @@ public class Control extends CodePart {
     String amount;
     String otherVar;
     String returnVarName;
+    String returnVarName2;
+    String returnVarName3;
 
     Control(ControlType controlType) {
         this.controlType = controlType;
@@ -48,7 +50,7 @@ public class Control extends CodePart {
     Control(ControlType controlType, String arg1, String arg2) {
         this.controlType = controlType;
         switch (controlType) {
-            case move, pathfind -> {
+            case move, pathfind, mine -> {
                 this.x = arg1;
                 this.y = arg2;
                 break;
@@ -56,6 +58,64 @@ public class Control extends CodePart {
             case itemDrop -> {
                 this.blockVarName = arg1;
                 this.amount = arg2;
+                break;
+            }
+            case targetp -> {
+                this.singleVar = arg1;
+                this.otherVar = arg2; // бля, так лень названия придумывать (C)DVD, 2023
+                break;
+            }
+        }
+    }
+
+    Control(ControlType controlType, String arg1, String arg2, String arg3) {
+        this.controlType = controlType;
+        switch (controlType) {
+            case approach, target -> {
+                this.x = arg1;
+                this.y = arg2;
+                this.otherVar = arg3;
+                break;
+            }
+            case itemTake -> {
+                this.blockVarName = arg1;
+                this.amount = arg2;
+                this.otherVar = arg3;
+                break;
+            }
+        }
+    }
+
+    Control(ControlType controlType, String arg1, String arg2, String arg3, String arg4) {
+        this.controlType = controlType;
+        switch (controlType) {
+            case within -> {
+                this.x = arg1;
+                this.y = arg2;
+                this.otherVar = arg3;
+                this.returnVarName = arg4;
+                break;
+            }
+        }
+    }
+
+    Control(ControlType controlType, String arg1, String arg2, String arg3, String arg4, String arg5) {
+        this.controlType = controlType;
+        switch (controlType) {
+            case build -> {
+                this.x = arg1;
+                this.y = arg2;
+                this.blockVarName = arg3;
+                this.singleVar = arg4;
+                this.otherVar = arg5;
+                break;
+            }
+            case getBlock -> {
+                this.x = arg1;
+                this.y = arg2;
+                this.returnVarName = arg3;
+                this.returnVarName2 = arg4;
+                this.returnVarName3 = arg5;
                 break;
             }
         }
